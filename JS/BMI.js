6e18b2ed625cdaf97d63bcc1e3f5935b1acf86b8
Faisal Alam <fazhd@hotmail.com>
2024-02-06 19:03:19 +0000

    added BMI Calculator

---
+++ JS/BMI.js
@@ -0,0 +1,33 @@
+let height;
+let weight;
+let total;
+
+document.getElementById("submitBtn").onclick = function () {
+  height = document.getElementById("height").value;
+  height = Number(height / 100);
+
+  weight = document.getElementById("weight").value;
+  weight = Number(weight);
+
+  height = Math.pow(height, 2);
+
+  total = weight / height;
+  let rounded = total.toFixed(1);
+
+  if (total >= 27.5) {
+    document.getElementById("BMI").innerHTML =
+      "Your BMI is: " + rounded + " You are obese";
+  } else if (total >= 23) {
+    document.getElementById("BMI").innerHTML =
+      "Your BMI is: " + rounded + " You are overweight";
+  } else if (total >= 18.5) {
+    document.getElementById("BMI").innerHTML =
+      "Your BMI is: " + rounded + " You are healthy";
+  } else if (total < 18.5) {
+    document.getElementById("BMI").innerHTML =
+      "Your BMI is: " + rounded + " You are underweight";
+  } else {
+    return (document.getElementById("BMI").innerHTML =
+      "Please enter a height and weight");
+  }
+};
