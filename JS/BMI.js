b6244891e04ca720f9e083fc420e7c84e721b1b7
Faisal Alam <fazhd@hotmail.com>
2024-02-06 22:32:03 +0000

    updated the obese part

--- JS/BMI.js
+++ JS/BMI.js
@@ -16,8 +16,10 @@ document.getElementById("submitBtn").onclick = function () {
 
   if (total >= 27.5) {
     document.getElementById("BMI").innerHTML =
-      "Your BMI is: " + rounded + " You are obese";
-    document.getElementById("hideImg").style.display = "block";
+      "Your BMI is: " +
+      rounded +
+      " You are obese go lose some weight u fat shit";
+    document.getElementById("obese").style.display = "block";
   } else if (total >= 23) {
     document.getElementById("BMI").innerHTML =
       "Your BMI is: " + rounded + " You are overweight";
