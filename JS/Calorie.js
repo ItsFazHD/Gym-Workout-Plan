a817c262d32ebd1f5e92e2dec95a7cc3f83da6de
Faisal Alam <fazhd@hotmail.com>
2024-02-07 15:51:03 +0000

    very active level completed

--- JS/Calorie.js
+++ JS/Calorie.js
@@ -90,17 +90,97 @@ document.getElementById("submitBtn").onclick = function () {
   }
 
   function maleModerate() {
-    maintenance = bmr * 1.375;
+    maintenance = bmr * 1.55;
     document.getElementById("maintenance").innerHTML = maintenance.toFixed(0);
 
     mildWeightLoss = maintenance * 0.89;
     document.getElementById("mildWeightLoss").innerHTML =
       mildWeightLoss.toFixed(0);
 
-    weightLoss = maintenance * 0.77;
+    weightLoss = maintenance * 0.79;
     document.getElementById("weightLoss").innerHTML = weightLoss.toFixed(0);
 
-    extremeWeightLoss = maintenance * 0.55;
+    extremeWeightLoss = maintenance * 0.58;
+    document.getElementById("extremeWeightLoss").innerHTML =
+      extremeWeightLoss.toFixed(0);
+  }
+
+  function femaleModerate() {
+    maintenance = bmr * 1.55;
+    document.getElementById("maintenance").innerHTML = maintenance.toFixed(0);
+
+    mildWeightLoss = maintenance * 0.88;
+    document.getElementById("mildWeightLoss").innerHTML =
+      mildWeightLoss.toFixed(0);
+
+    weightLoss = maintenance * 0.76;
+    document.getElementById("weightLoss").innerHTML = weightLoss.toFixed(0);
+
+    extremeWeightLoss = maintenance * 0.53;
+    document.getElementById("extremeWeightLoss").innerHTML =
+      extremeWeightLoss.toFixed(0);
+  }
+
+  function maleVery() {
+    maintenance = bmr * 1.725;
+    document.getElementById("maintenance").innerHTML = maintenance.toFixed(0);
+
+    mildWeightLoss = maintenance * 0.91;
+    document.getElementById("mildWeightLoss").innerHTML =
+      mildWeightLoss.toFixed(0);
+
+    weightLoss = maintenance * 0.82;
+    document.getElementById("weightLoss").innerHTML = weightLoss.toFixed(0);
+
+    extremeWeightLoss = maintenance * 0.64;
+    document.getElementById("extremeWeightLoss").innerHTML =
+      extremeWeightLoss.toFixed(0);
+  }
+
+  function femaleVery() {
+    maintenance = bmr * 1.725;
+    document.getElementById("maintenance").innerHTML = maintenance.toFixed(0);
+
+    mildWeightLoss = maintenance * 0.9;
+    document.getElementById("mildWeightLoss").innerHTML =
+      mildWeightLoss.toFixed(0);
+
+    weightLoss = maintenance * 0.8;
+    document.getElementById("weightLoss").innerHTML = weightLoss.toFixed(0);
+
+    extremeWeightLoss = maintenance * 0.6;
+    document.getElementById("extremeWeightLoss").innerHTML =
+      extremeWeightLoss.toFixed(0);
+  }
+
+  function maleExtra() {
+    maintenance = bmr * 1.9;
+    document.getElementById("maintenance").innerHTML = maintenance.toFixed(0);
+
+    mildWeightLoss = maintenance * 0.92;
+    document.getElementById("mildWeightLoss").innerHTML =
+      mildWeightLoss.toFixed(0);
+
+    weightLoss = maintenance * 0.84;
+    document.getElementById("weightLoss").innerHTML = weightLoss.toFixed(0);
+
+    extremeWeightLoss = maintenance * 0.67;
+    document.getElementById("extremeWeightLoss").innerHTML =
+      extremeWeightLoss.toFixed(0);
+  }
+
+  function femaleExtra() {
+    maintenance = bmr * 1.9;
+    document.getElementById("maintenance").innerHTML = maintenance.toFixed(0);
+
+    mildWeightLoss = maintenance * 0.91;
+    document.getElementById("mildWeightLoss").innerHTML =
+      mildWeightLoss.toFixed(0);
+
+    weightLoss = maintenance * 0.82;
+    document.getElementById("weightLoss").innerHTML = weightLoss.toFixed(0);
+
+    extremeWeightLoss = maintenance * 0.64;
     document.getElementById("extremeWeightLoss").innerHTML =
       extremeWeightLoss.toFixed(0);
   }
@@ -129,5 +209,29 @@ document.getElementById("submitBtn").onclick = function () {
   ) {
     femaleBmr();
     femaleLittle();
+  } else if (
+    document.getElementById("male").checked == true &&
+    document.getElementById("moderate").checked == true
+  ) {
+    maleBmr();
+    maleModerate();
+  } else if (
+    document.getElementById("female").checked == true &&
+    document.getElementById("moderate").checked == true
+  ) {
+    femaleBmr();
+    femaleModerate();
+  } else if (
+    document.getElementById("male").checked == true &&
+    document.getElementById("veryActive").checked == true
+  ) {
+    maleBmr();
+    maleVery();
+  } else if (
+    document.getElementById("female").checked == true &&
+    document.getElementById("veryActive").checked == true
+  ) {
+    femaleBmr();
+    femaleVery();
   }
 };
