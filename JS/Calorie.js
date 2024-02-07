3a293c0c178c7f313a14b6610aacd457f2831d73
Faisal Alam <fazhd@hotmail.com>
2024-02-07 15:19:18 +0000

    Sedentary level completed

--- JS/Calorie.js
+++ JS/Calorie.js
@@ -9,7 +9,9 @@ document.getElementById("submitBtn").onclick = function () {
   gender = document.querySelector("input[name = gender]:checked").value;
   height = document.getElementById("height").value;
   weight = document.getElementById("weight").value;
-  //activityLevel = document.querySelector("activityLevel").value;
+  activityLevel = document.querySelector(
+    "input[name = activity]:checked"
+  ).value;
 
   function maleBmr() {
     maleBmr = 10 * weight + 6.25 * height - 5 * age + 5;
@@ -23,13 +25,51 @@ document.getElementById("submitBtn").onclick = function () {
     document.getElementById("bmr").innerHTML = bmr;
   }
 
-  if (document.getElementById("male").checked == true) {
+  function maleSedentary() {
+    maintenance = bmr * 1.2;
+    document.getElementById("maintenance").innerHTML = maintenance.toFixed(0);
+
+    mildWeightLoss = maintenance * 0.87;
+    document.getElementById("mildWeightLoss").innerHTML =
+      mildWeightLoss.toFixed(0);
+
+    weightLoss = maintenance * 0.74;
+    document.getElementById("weightLoss").innerHTML = weightLoss.toFixed(0);
+
+    extremeWeightLoss = maintenance * 0.48;
+    document.getElementById("extremeWeightLoss").innerHTML =
+      extremeWeightLoss.toFixed(0);
+  }
+
+  function femaleSedentary() {
+    maintenance = bmr * 1.2;
+    document.getElementById("maintenance").innerHTML = maintenance.toFixed(0);
+
+    mildWeightLoss = maintenance * 0.86;
+    document.getElementById("mildWeightLoss").innerHTML =
+      mildWeightLoss.toFixed(0);
+
+    weightLoss = maintenance * 0.71;
+    document.getElementById("weightLoss").innerHTML = weightLoss.toFixed(0);
+
+    extremeWeightLoss = maintenance * 0.42;
+    document.getElementById("extremeWeightLoss").innerHTML =
+      extremeWeightLoss.toFixed(0);
+  }
+
+  if (
+    document.getElementById("male").checked == true &&
+    document.getElementById("sedentary").checked == true
+  ) {
     maleBmr();
-  } else if (document.getElementById("female").checked == true) {
+    maleSedentary();
+  } else if (
+    document.getElementById("female").checked == true &&
+    document.getElementById("sedentary").checked == true
+  ) {
     femaleBmr();
+    femaleSedentary();
   } else {
     document.getElementById("bmr").innerHTML = "Please enter your details";
   }
 };
-
-//document.getElementById("hiddenTable").style.display = "block";
